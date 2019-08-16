69c8d81e1af4a78d4b9764e95f14795af98ba3b8
Aishwarya <aishwaryasachdeva20@gmail.com>
2019-08-16 14:44:12 +0530

    Refactored some code

--- src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,6 +3,8 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,19 +15,30 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
+	private List<Student> theStudent ;
+	
 	// define endpoint for "/students " - this will return the list of student
 	
-	@GetMapping("/students")
-	public List<Student> getStudent()
+	@PostConstruct
+	public void loadData()
+	
 	{
-		List<Student> theStudent = new ArrayList<Student>();
+		
+		System.out.println("I am loaddata");
+theStudent = new ArrayList<Student>();
 		
 		theStudent.add(new Student("A","B"));
 		theStudent.add(new Student("X","Y"));
 		theStudent.add(new Student("G","H"));
+		System.out.println("I have loaded data");
 		
-		
-		return theStudent;
+	}
+	
+	
+	@GetMapping("/students")
+	public List<Student> getStudent()
+	{
+	     return theStudent;
 		
 	}
 	
