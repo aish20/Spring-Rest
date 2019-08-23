607e52fe1e0d91a725b7147ccafaa67dd37eae9f
Aishwarya <aishwaryasachdeva20@gmail.com>
2019-08-23 16:54:28 +0530

    Added code for exception handling

--- src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +43,50 @@ theStudent = new ArrayList<Student>();
 	public List<Student> getStudent()
 	{
 	     return theStudent;
+		
+	}
+	
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId)
+	{
+		
+		// just index into the list and this will return that particular student name
+		
+		// check the studentId against the list size and this will throw the error 
+		
+		if((studentId>=theStudent.size())|| (studentId <0))
+		{
+			throw new StudentNotFoundException("Student id is not found = "+studentId);
+		}
+		return theStudent.get(studentId);
+		
+	}
+	
+	// Add an exception handler using @ExceptionHandler annotation
+	
+	
+	// this is the exception handler method 
+	
+	// StudentErrorResponse is the type of response body 
+	
+	// StudentNotFoundException exception type to be catched or handled
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exception)
+	{
+		// create a StudentErrorResponse 
+		
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		
+		error.setMessage(exception.getMessage());
+		
+		error.setTimestamp(System.currentTimeMillis());
+		
+		// return responseEntity 
+		return new ResponseEntity<StudentErrorResponse>(error,HttpStatus.NOT_FOUND);
+		
 		
 	}
 	
